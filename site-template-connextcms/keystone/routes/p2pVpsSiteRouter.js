864e688663d14e867976f82b4796bb097f76c449
chris.troutner <chris.troutner@gmail.com>
2017-10-13 17:39:08 -0700

    Completed /api/devicePublicData/listById/:id api

--- site-template-connextcms/keystone/routes/p2pVpsSiteRouter.js
+++ site-template-connextcms/keystone/routes/p2pVpsSiteRouter.js
@@ -18,18 +18,19 @@ module.exports = function(app) {
   app.get('/api/portcontrol/list', keystone.middleware.api, routes.api.portcontrol.list);
   app.all('/api/portcontrol/create', keystone.middleware.api, routes.api.portcontrol.create);
   app.all('/api/portcontrol/:id/update', keystone.middleware.api, routes.api.portcontrol.update);
-	app.get('/api/portcontrol/:id/remove', keystone.middleware.api, routes.api.portcontrol.remove);
+  app.get('/api/portcontrol/:id/remove', keystone.middleware.api, routes.api.portcontrol.remove);
   
   app.get('/api/devicePublicData/list', keystone.middleware.api, routes.api.devicePublicData.list);
   app.all('/api/devicePublicData/create', keystone.middleware.api, routes.api.devicePublicData.create);
   app.all('/api/devicePublicData/:id/update', keystone.middleware.api, routes.api.devicePublicData.update);
-	app.get('/api/devicePublicData/:id/remove', keystone.middleware.api, routes.api.devicePublicData.remove);
+  app.get('/api/devicePublicData/:id/remove', keystone.middleware.api, routes.api.devicePublicData.remove);
   app.all('/api/devicePublicData/:id/register', keystone.middleware.api, routes.api.devicePublicData.register);
+  app.get('/api/devicePublicData/listById/:id', keystone.middleware.api, routes.api.devicePublicData.listById);
   
   app.get('/api/devicePrivateData/list', keystone.middleware.api, routes.api.devicePrivateData.list);
   app.all('/api/devicePrivateData/create', keystone.middleware.api, routes.api.devicePrivateData.create);
   app.all('/api/devicePrivateData/:id/update', keystone.middleware.api, routes.api.devicePrivateData.update);
-	app.get('/api/devicePrivateData/:id/remove', keystone.middleware.api, routes.api.devicePrivateData.remove);
+  app.get('/api/devicePrivateData/:id/remove', keystone.middleware.api, routes.api.devicePrivateData.remove);
   
   
   // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
