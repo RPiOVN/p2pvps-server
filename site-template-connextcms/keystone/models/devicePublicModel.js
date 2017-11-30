786bff54191019426e8fc02ae0f15298d3bfd8c6
Chris Troutner <chris.troutner@gmail.com>
2017-11-29 17:18:52 -0800

    changed devicePublicModel

--- site-template-connextcms/keystone/models/devicePublicModel.js
+++ site-template-connextcms/keystone/models/devicePublicModel.js
@@ -1,20 +1,20 @@
-var keystone = require('keystone');
-var Types = keystone.Field.Types;
+var keystone = require('keystone')
+var Types = keystone.Field.Types
 
 /**
  * Device Public Model
  * ==================
  */
 
-var DevicePublicModel = new keystone.List('DevicePublicModel');
+var DevicePublicModel = new keystone.List('DevicePublicModel')
 
 DevicePublicModel.add({
   ownerUser: { type: Types.Relationship, ref: 'User' },
   renterUser: { type: Types.Relationship, ref: 'User' },
   privateData: { type: Types.Relationship, ref: 'DevicePrivateModel' },
-  obContract: { type: Types.Relationship, ref: 'obContractModel'},
+  obContract: { type: Types.Relationship, ref: 'obContractModel' },
   rentStartDate: { type: String },
-  rentStopDate: { type: String },
+  expiration: { type: String }, // Date client should be reset.
   deviceName: { type: String },
   deviceDesc: { type: String },
   rentHourlyRate: { type: String },
@@ -22,11 +22,11 @@ DevicePublicModel.add({
   httpPort: { type: String },
   sshPort: { type: String },
   memory: { type: String },
-  diskSpace:  { type: String },
-  processor:  { type: String },
-  internetSpeed:  { type: String },
+  diskSpace: { type: String },
+  processor: { type: String },
+  internetSpeed: { type: String },
   checkinTimeStamp: { type: String }
-});
+})
 
-DevicePublicModel.defaultColumns = 'ownerUser';
-DevicePublicModel.register();
+DevicePublicModel.defaultColumns = 'ownerUser'
+DevicePublicModel.register()
