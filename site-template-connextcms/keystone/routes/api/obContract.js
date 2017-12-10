ad52cef33ad42ff1a7a1085dfdcd332d861ad71d
Chris Troutner <chris.troutner@gmail.com>
2017-12-10 13:21:56 -0800

    Creating OB listing on Client registration

--- site-template-connextcms/keystone/routes/api/obContract.js
+++ site-template-connextcms/keystone/routes/api/obContract.js
@@ -1,131 +1,114 @@
 var keystone = require('keystone');
-var request = require('request');
-var Promise = require('node-promise'); //Promises to handle asynchonous callbacks.
+// var request = require('request');
+// var Promise = require('node-promise'); // Promises to handle asynchonous callbacks.
 
-
-var DevicePublicModel = keystone.list('DevicePublicModel');
-var DevicePrivateModel = keystone.list('DevicePrivateModel');
+// var DevicePublicModel = keystone.list('DevicePublicModel');
+// var DevicePrivateModel = keystone.list('DevicePrivateModel');
 var obContractModel = keystone.list('obContractModel');
 
 /**
  * List obContractModel
  */
-exports.list = function(req, res) {
-  obContractModel.model.find(function(err, items) {
-
+exports.list = function (req, res) {
+  obContractModel.model.find(function (err, items) {
     if (err) return res.apiError('database error', err);
 
     res.apiResponse({
-        collection: items
+      collection: items
     });
-
   });
 }
-
-
 
 /**
  * Create obContractModel
  */
-exports.create = function(req, res) {
-	//debugger;
+exports.create = function (req, res) {
+  debugger;
 
-  //Ensure the user has a valid CSRF token
-	//if (!security.csrf.validate(req)) {
-	//	return res.apiError(403, 'invalid csrf');
-	//}
+  // Ensure the user has a valid CSRF token
+  // if (!security.csrf.validate(req)) {
+  //  return res.apiError(403, 'invalid csrf');
+  // }
 
-	var item = new obContractModel.model(),
-		data = (req.method == 'POST') ? req.body : req.query;
+  var item = new obContractModel.model();
+  var data = (req.method === 'POST') ? req.body : req.query;
 
-	item.getUpdateHandler(req).process(data, function(err) {
+  item.getUpdateHandler(req).process(data, function (err) {
+    if (err) return res.apiError('error', err);
 
-		if (err) return res.apiError('error', err);
-
-		res.apiResponse({
-			collection: item
-		});
-
-	});
-
+    res.apiResponse({
+      collection: item
+    });
+  });
 }
 
-exports.get = function(req, res) {
-
-  obContractModel.model.findById(req.params.id).exec(function(err, item) {
-
+exports.get = function (req, res) {
+  obContractModel.model.findById(req.params.id).exec(function (err, item) {
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
 
     res.apiResponse({
       collection: item
     });
-
   });
 }
 
 /**
  * Update DevicePrivateModel by ID
  */
-exports.update = function(req, res) {
+exports.update = function (req, res) {
+  // Ensure the user has a valid CSRF token
+  // if (!security.csrf.validate(req)) {
+  //  return res.apiError(403, 'invalid csrf');
+  // }
 
-  //Ensure the user has a valid CSRF token
-	//if (!security.csrf.validate(req)) {
-	//	return res.apiError(403, 'invalid csrf');
-	//}
-
-  //Ensure the user making the request is a Keystone Admin
-  //var isAdmin = req.user.get('isAdmin');
-  //if(!isAdmin) {
+  // Ensure the user making the request is a Keystone Admin
+  // var isAdmin = req.user.get('isAdmin');
+  // if(!isAdmin) {
   //  return res.apiError(403, 'Not allowed to access this API. Not Keystone Admin.');
-  //}
+  // }
 
-  //Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
-  //This is a check to make sure the user is a ConnexstCMS Admin
-  //var admins = keystone.get('admins');
-  //var userId = req.user.get('id');
-  //if(admins.indexOf(userId) == -1) {
+  // Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
+  // This is a check to make sure the user is a ConnexstCMS Admin
+  // var admins = keystone.get('admins');
+  // var userId = req.user.get('id');
+  // if(admins.indexOf(userId) == -1) {
   //  return res.apiError(403, 'Not allowed to access this API. Not ConnextCMS Admin')
-  //}
+  // }
 
-	obContractModel.model.findById(req.params.id).exec(function(err, item) {
+  obContractModel.model.findById(req.params.id).exec(function (err, item) {
+    if (err) return res.apiError('database error', err);
+    if (!item) return res.apiError('not found');
 
-		if (err) return res.apiError('database error', err);
-		if (!item) return res.apiError('not found');
+    var data = (req.method === 'POST') ? req.body : req.query;
 
-		var data = (req.method == 'POST') ? req.body : req.query;
+    item.getUpdateHandler(req).process(data, function (err) {
+      if (err) return res.apiError('create error', err);
 
-		item.getUpdateHandler(req).process(data, function(err) {
-
-			if (err) return res.apiError('create error', err);
-
-			res.apiResponse({
-				collection: item
-			});
-
-		});
-
-	});
+      res.apiResponse({
+        collection: item
+      });
+    });
+  });
 }
 
 /**
  * Delete DevicePrivateModel by ID
  */
-exports.remove = function(req, res) {
+exports.remove = function (req, res) {
+  // Ensure the user has a valid CSRF token
+  // if (!security.csrf.validate(req)) {
+  //  return res.apiError(403, 'invalid csrf');
+  // }
 
-  //Ensure the user has a valid CSRF token
-	//if (!security.csrf.validate(req)) {
-	//	return res.apiError(403, 'invalid csrf');
-	//}
-
-  //Ensure the user making the request is a Keystone Admin
-  //var isAdmin = req.user.get('isAdmin');
-  //if(!isAdmin) {
+  // Ensure the user making the request is a Keystone Admin
+  // var isAdmin = req.user.get('isAdmin');
+  // if(!isAdmin) {
   //  return res.apiError(403, 'Not allowed to access this API. Not Keystone Admin.');
-  //}
+  // }
 
-  //Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
-  //This is a check to make sure the user is a ConnexstCMS Admin
+  // Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
+  // This is a check to make sure the user is a ConnexstCMS Admin
   /*
   var admins = keystone.get('admins');
   var userId = req.user.get('id');
@@ -135,17 +118,15 @@ exports.remove = function(req, res) {
   */
 
   obContractModel.model.findById(req.params.id).exec(function (err, item) {
+    if (err) return res.apiError('database error', err);
+    if (!item) return res.apiError('not found');
 
-		if (err) return res.apiError('database error', err);
-		if (!item) return res.apiError('not found');
+    item.remove(function (err) {
+      if (err) return res.apiError('database error', err);
 
-		item.remove(function (err) {
-			if (err) return res.apiError('database error', err);
-
-			return res.apiResponse({
-				success: true
-			});
-		});
-
-	});
+      return res.apiResponse({
+        success: true
+      });
+    });
+  });
 }
